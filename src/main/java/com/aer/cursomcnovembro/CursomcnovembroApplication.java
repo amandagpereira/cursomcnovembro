3e8d0887648088410eafc619c316f48a4aeca47c
Amanda Gaspar <amandagpereira@gmail.com>
2020-11-27 11:27:37 -0300

    Projeto criado

---
+++ src/main/java/com/aer/cursomcnovembro/CursomcnovembroApplication.java
@@ -0,0 +1,13 @@
+package com.aer.cursomcnovembro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CursomcnovembroApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CursomcnovembroApplication.class, args);
+	}
+
+}
